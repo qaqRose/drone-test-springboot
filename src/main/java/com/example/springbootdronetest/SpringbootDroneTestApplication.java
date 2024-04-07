9ec2d4045099f3edc548ee44f70f97a47432d89c
qaqRose <singleqaq@gmail.com>
2024-04-07 13:44:02 +0800

    commit

---
+++ src/main/java/com/example/springbootdronetest/SpringbootDroneTestApplication.java
@@ -0,0 +1,21 @@
+package com.example.springbootdronetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SpringbootDroneTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDroneTestApplication.class, args);
+    }
+
+    @GetMapping
+    public String hello() {
+        return "hello drone";
+    }
+
+}
